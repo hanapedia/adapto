5e41011a539b0c4c3f0beca1acac16d821cb5d79
Hiroki <hiroki11hanada@gmail.com>
2024-12-03 15:49:59 +0900

    fix: update overload threshold continuously

--- rto/rto.go
+++ rto/rto.go
@@ -400,15 +400,17 @@ func (arp *AdaptoRTOProvider) onInterval() {
 			// TODO: this only works when optimum is higher than current estimate
 			// only for load increase and not capacity decrease
 			arp.pacingGain++
+			arp.overloadThresholdReq -= arp.overloadThresholdReq >> arp.pacingGain
 			arp.sendRateInterval = arp.interval / time.Duration(
-				arp.overloadThresholdReq-arp.overloadThresholdReq>>arp.pacingGain,
+				arp.overloadThresholdReq,
 			)
 			arp.logger.Info("still in overload, shrinking pacing", "sendRateInterval", arp.sendRateInterval, "overloadThresholdReq", arp.overloadThresholdReq, "dropped", arp.dropped, "req", arp.req)
 		} else {
 			// gain pacing by x1.125
 			// TODO: consider resetting pacing gain or cycling
+			arp.overloadThresholdReq += arp.overloadThresholdReq >> arp.pacingGain
 			arp.sendRateInterval = arp.interval / time.Duration(
-				arp.overloadThresholdReq+arp.overloadThresholdReq>>arp.pacingGain,
+				arp.overloadThresholdReq,
 			)
 			arp.logger.Info("still in overload, growing pacing", "sendRateInterval", arp.sendRateInterval, "overloadThresholdReq", arp.overloadThresholdReq, "dropped", arp.dropped, "req", arp.req)
 		}

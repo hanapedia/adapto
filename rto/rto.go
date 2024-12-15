b40a8ae085503c12becba878a3a25625c0f98e79
Hiroki <hiroki11hanada@gmail.com>
2024-12-15 23:25:55 +0900

    feat: ease minimum samples required

--- rto/rto.go
+++ rto/rto.go
@@ -28,7 +28,7 @@ const (
 	// Tunable parameters
 	// The values set for these constant leaves room for tuning
 	SLO_SAFETY_MARGIN                float64 = 0.5 // safety margin of 0.5 or division by 2
-	MIN_FAILED_SAMPLES               float64 = 2   // minimum failed samples reqruired to compute failure rate
+	MIN_FAILED_SAMPLES               float64 = 1   // minimum failed samples reqruired to compute failure rate
 	LOG2_PACING_GAIN                 int64   = 5   // used as pacing gain factor. 1+ 1 >> LOG2_PACING_GAIN
 	DEFAULT_OVERLOAD_DRAIN_INTERVALS uint64  = 2   // intervals to choke rto after overload. TODO: reduce this
 )
